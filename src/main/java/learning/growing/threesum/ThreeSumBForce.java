c5a14ff2d181e7968ddc1ee65c4ccbc2a9c2daa0
bb-mohansarilla <mohanarao.sarilla@bigbasket.com>
2022-06-07 15:32:23 +0530

    ThreeSumBForce: Added bruteforce approach

--- src/main/java/learning/growing/threesum/ThreeSumBForce.java
+++ src/main/java/learning/growing/threesum/ThreeSumBForce.java
@@ -3,7 +3,7 @@ package learning.growing.threesum;
 public class ThreeSumBForce {
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5,6,7,8,9};
-        int targetSum = 1;
+        int targetSum = 10;
         boolean isExists = threeSumExists(arr, targetSum);
 
         if (!isExists) {
@@ -15,9 +15,10 @@ public class ThreeSumBForce {
 
     private static boolean threeSumExists(int[] arr, int targetSum) {
         for (int i = 0; i < arr.length-2; i++) {
-            for (int j = 0; j < arr.length-1; j++) {
-                for (int k = 0; k < arr.length; k++) {
+            for (int j = i+1; j < arr.length-1; j++) {
+                for (int k = j+1; k < arr.length; k++) {
                     if (arr[i] + arr[j] + arr[k] == targetSum) {
+                        System.out.format("Three Sum Exists: [%d , %d , %d]", arr[i], arr[j], arr[k]);
                         return true;
                     }
                 }
